39305cf0c421345c66f4fdd9f484007dbad4c6c5
xnought <bertuccd@oregonstate.edu>
2024-08-07 16:01:42 -0400

    feat: setup for the add function

---
+++ src/main.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+
+const DigitError = error{NaN};
+
+fn isDigit(char: u8) bool {
+    return (char == '0') or (char == '1') or (char == '2') or (char == '3') or
+        (char == '4') or (char == '5') or (char == '6') or (char == '7') or (char == '8') or (char == '9');
+}
+fn isIntegerStr(str: []const u8) DigitError!void {
+    for (str) |char| {
+        if (!isDigit(char)) {
+            return DigitError.NaN;
+        }
+    }
+}
+
+fn stringAdd(str_a: []const u8, str_b: []const u8) ![]const u8 {
+    // error out if the strings aren't valid integers to add together
+    try isIntegerStr(str_a);
+    try isIntegerStr(str_b);
+
+    // now do the string addition
+
+    return "Hello!";
+}
+
+pub fn main() !void {
+    const str_a = "12341";
+    const res = try stringAdd(str_a, str_a);
+    std.debug.print("{s}", .{res});
+}
